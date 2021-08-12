d8da9fbf9ac64febef742ce7b8dc3bdaab1b4359
Matteo Crippa <matteocrippa@users.noreply.github.com>
2021-08-12 14:47:48 +0200

    clean preview handling

--- Project/Controller/Map/MapViewController+Preview.swift
+++ Project/Controller/Map/MapViewController+Preview.swift
@@ -10,50 +10,19 @@ import Foundation
 import ProximiioMapbox
 import Proximiio
 
-// TODO: replace adding your parking identifier
-let parkingIdentifier = "REPLACE-ME"
-
 // MARK: - Preview handler
 extension MapViewController {
-    internal func showPreview(feature: ProximiioGeoJSON?, parking: Bool = false) {
+    internal func showPreview(feature: ProximiioGeoJSON?) {
             
         // calculate
         guard let feature = feature else {
             return
         }
-        
-        var waypoints: [PIOWaypoint] = []
-        
-        hideAnnotationPark()
-        
-        if parking, let coordinate = feature.coordinate {
-                        
-            let closestParking = PIODatabase.sharedInstance().features()
-                .filter {
-                    $0.amenity == parkingIdentifier
-                }
-                .sortedByDistance(from: coordinate).first
-            
-            let parkAnnotation = ParkAnnotation()
-            parkAnnotation.level = 0
 
-            if let parking = closestParking {
-                if let parkCoords = closestParking?.coordinate {
-                    parkAnnotation.coordinate = parkCoords
-                }
-                waypoints.append(SimpleWaypoint(feature: parking))
-            }
-
-            if let image = UIImage(named: "parking_highlight") {
-                parkAnnotation.image = image
-            }
-            mapView?.addAnnotation(parkAnnotation)
-        }
-        
         let configuration = PIORouteConfiguration(
             start: nil,
             destination: feature,
-            waypointList: waypoints,
+            waypointList: [],
             wayfindingOptions: PIOWayfindingOptions.optionsConfiguration
         )
         
@@ -73,7 +42,7 @@ extension MapViewController {
                 self?.previewView.viewModel.route = route
                 
                 self?.previewView.snp.updateConstraints {
-                    $0.height.equalTo(282)
+                    $0.height.equalTo(242)
                 }
                 self?.previewView.buttonClose.isHidden = false
             }
@@ -81,30 +50,21 @@ extension MapViewController {
     }
 
     internal func hidePreview() {
-        isPreview = false
+        DispatchQueue.main.async { [weak self] in
+            self?.isPreview = false
 
-        previewView.snp.updateConstraints {
-            $0.height.equalTo(0)
-        }
-        previewView.buttonClose.isHidden = true
-        view.layoutSubviews()
-        mapUIOverlayFooter.hide(navigation: true, search: false, nearby: false)
-    }
-    
-    fileprivate func hideAnnotationPark() {
-        self.mapView?.annotations?.forEach {
-            if let annotation = $0 as? ParkAnnotation, annotation.isParking {
-                DispatchQueue.main.async {
-                    self.mapView?.removeAnnotation(annotation)
-                }
+            self?.previewView.snp.updateConstraints {
+                $0.height.equalTo(0)
             }
+            self?.previewView.buttonClose.isHidden = true
+            self?.view.layoutSubviews()
+            self?.mapUIOverlayFooter.hide(navigation: true, search: false, nearby: false)
         }
     }
     
     func hideAnnotations() {
         DispatchQueue.main.async {
             ProximiioMapbox.shared.hidePreviewDestinationMarker()
-            self.hideAnnotationPark()
         }
     }
 }
@@ -124,12 +84,19 @@ extension MapViewController {
     }
 
     internal func previewCancel() {
-        previewView.reset()
-        hideAnnotations()
         hidePreview()
-        ProximiioMapbox.shared.mapNavigation = self
-        ProximiioMapbox.shared.mapInteraction = self
-        ProximiioMapbox.shared.routeCancel(silent: true)
-        mapUIOverlayFooter.hide(navigation: true, search: false, nearby: false)
+        previewView.reset()
+
+//        DispatchQueue.main.async { [weak self] in
+//            self?.hidePreview()
+//            // ProximiioMapbox.shared.mapNavigation = self
+//            // ProximiioMapbox.shared.mapInteraction = self
+//            // self?.mapUIOverlayFooter.hide(navigation: true, search: false, nearby: false)
+//            self?.previewView.reset()
+//        }
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            self?.hideAnnotations()
+            ProximiioMapbox.shared.routeCancel(silent: true)
+        }
     }
 }
